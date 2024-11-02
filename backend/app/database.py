f0c7cadefd4e72c63a030dfa9b23803daa49d621
Charalampos KERAMIDAS <ckeramidas@littlebigcode.be>
2024-11-02 12:50:38 +0100

    fix: db user working

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/fishdb")
+print(f"Using DATABASE_URL: {DATABASE_URL}")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
